e573a79f82f954a5d54051366ac9583ed4830a34
vishaal <28676655+vss2sn@users.noreply.github.com>
2019-02-17 16:12:14 -0500

    Minor debug

--- src/dijstra.cpp
+++ src/dijstra.cpp
@@ -5,29 +5,13 @@ Dijstra grid based planning
 */
 
 #include "main.h"
-
-struct point {
-  int x,y;
-  point operator+(point p){
-    std::cout << "In overloaded operator +" << std::endl;
-    point tmp;
-    tmp.x = this->x + p.x;
-    tmp.y = this->y + p.y;
-    std::cout << "About to return" << std::endl;
-    return tmp;
-  }
-  point operator=(point p){
-    std::cout << "In overloaded operator =" << std::endl;
-    this->x = p.x;
-    this->y = p.y;
-  }
-};
-
+#include <iomanip>
 class Node{
 //private:
-
 public:
+
   int x, y, cost, pind;
+
   Node(int x = 0, int y = 0, int cost = 0, int pind = 0){
     this->x = x;
     this->y = y;
@@ -35,11 +19,13 @@ public:
     this->pind = pind;
   }
   void print_status(){
-    std::cout << "Node:" << std::endl
+    std::cout << "--------------" << std::endl
+              << "Node:" << std::endl
               << "x   : " << x    << std::endl
               << "y   : " << y    << std::endl
               << "cost: " << cost << std::endl
-              << "pind: " << pind << std::endl;
+              << "pind: " << pind << std::endl
+              << "--------------" << std::endl;
   }
   Node operator+(Node p){
     Node tmp;
@@ -55,26 +41,31 @@ public:
     this->cost = p.cost;
     this->pind = p.pind;
   }
-
 };
 
 void make_grid(void *grid, int n){
   //NOTE: Using a void pointer isnt the best option
   int (*p_grid)[n][n] = (int (*)[n][n]) grid;
-  std::srand(10);
+
+  std::random_device rd; // obtain a random number from hardware
+  std::mt19937 eng(rd()); // seed the generator
+  std::uniform_int_distribution<int> distr(0,4); // define the range
+
   for(int i=0;i<n;i++){
     for(int j=0;j<n;j++){
-      *p_grid[i][j] = (std::rand() % 4)/3;
+      (*p_grid)[i][j] = distr(eng)/3;
     }
   }
 }
 
 void print_grid(void *grid, int n){
   //NOTE: Using a void pointer isnt the best option
+
   int (*p_grid)[n][n] = (int (*)[n][n]) grid;
   for(int i=0;i<n;i++){
     for(int j=0;j<n;j++){
-      std::cout << *p_grid[i][j] << " , ";
+      //std::cout << std::setw(10) << (*p_grid)[i][j] << " , ";
+      std::cout << (*p_grid)[i][j] << " , ";
     }
     std::cout << std::endl;
   }
@@ -82,22 +73,14 @@ void print_grid(void *grid, int n){
 
 int main(){
 /*
-  Node a;
-  Node b;
-  a.x = 1;
-  a.y = 1;
-  b.x = 1;
-  b.y = 1;
-  std::cout << "Done with init" <<std::endl;
+  Node a(1,1,0,0);
+  Node b(2,1,0,0);
   Node c;
   c=a+b;
-  std::cout << c.x <<std::endl;
-  std::cout << c.y <<std::endl;
+  c.print_status();
   c=a;
-  std::cout << c.x <<std::endl;
-  std::cout << c.y <<std::endl;
+  c.print_status();
 */
-
   int n = 10;
   int num_points = n*n;
 //  point sptSet[num_points];
