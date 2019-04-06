71be93858d72c1266b0d57aaa5e8ef0f89c08894
vishaal <28676655+vss2sn@users.noreply.github.com>
2019-04-06 08:28:39 -0400

    Minor edits to allow building with test.

--- inc/main.hpp
+++ inc/main.hpp
@@ -32,10 +32,12 @@
 #include <cmath>
 #include <thread>
 
-#ifndef BUILD_INDIVIDUAL
+#ifndef BUILD_INDIVIDUAL // Only to make sure main is is included in documentation
+#ifndef TEST             // Only to make sure main is is included in documentation
 /**
 * @brief Script main function. Generates start and end nodes as well as grid, then creates the algorithm objects and calls the main algorithm functions.
 * @return 0
 */
 int main();
+#endif
 #endif
