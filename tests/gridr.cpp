5081102d1f794062ff3290b33148955e6ad360c7
vishaal <28676655+vss2sn@users.noreply.github.com>
2019-07-07 17:16:10 -0400

    Minor debug.

--- tests/gridr.cpp
+++ tests/gridr.cpp
@@ -18,7 +18,7 @@ double run_test(std::vector<std::vector<int> > &grid, std::string algo){
   //Make sure start and goal are not obstacles and their ids are correctly assigned.
   grid[start.x_][start.y_] = 0;
   grid[goal.x_][goal.y_] = 0;
-
+  PrintGrid(grid);
   std::vector<Node> path_vector;
   // Variables for RRT and RRTStar
   double threshold = 2;
@@ -79,11 +79,12 @@ TEST(PathPlanningTest, Test1) {
   std::vector<std::vector<int>> grid_5 = grid_1;
   std::vector<std::vector<int>> grid_6 = grid_1;
   std::vector<std::vector<int>> grid_7 = grid_1;
+  std::vector<std::vector<int>> grid_8 = grid_1;
 
   ASSERT_EQ(run_test(grid_1, "dijkstra"), run_test(grid_2, "a_star"));
   ASSERT_EQ(run_test(grid_3, "lpa_star"), run_test(grid_4, "a_star"));
   ASSERT_EQ(run_test(grid_5, "d_star_lite"), run_test(grid_6, "a_star"));
-  ASSERT_GE(abs(run_test(grid_6, "a_star")*1.2), abs(run_test(grid_7, "ant_colony")));
+  ASSERT_GE(abs(run_test(grid_7, "a_star")*1.2), abs(run_test(grid_8, "ant_colony")));
 }
 
 int main(int argc, char **argv) {
